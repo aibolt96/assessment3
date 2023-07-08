d596e1e3db5f6962efd6492142b37d8c2c6ac3bb
Austin Bolton <aibolt3@gmail.com>
2023-07-08 11:32:54 -0500

    patrs 1 - 3

--- about.js
+++ about.js
@@ -5,10 +5,17 @@ console.log("hello world");
 function handleSubmit(evt) {
 	evt.preventDefault();
 	
-	console.log('form submit');
+	alert('Form Submitted');
 }
 
+function compliment(evt) {
+	evt.preventDefault();
+	
+	alert('You are doing great!');
+}
 
+let img = document.getElementById('catImg')
 let form = document.querySelector('#contact');
 
 form.addEventListener('submit', handleSubmit);
+img.addEventListener('mouseover', compliment)
