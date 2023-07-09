e69333fc91062d6363c5624b9e0aaec910fa8e58
Austin Bolton <aibolt3@gmail.com>
2023-07-08 20:39:28 -0500

    finished parts 4-5

---
+++ profile.js
@@ -0,0 +1,10 @@
+document.getElementById('color').addEventListener('click', function(){
+    alert('My favorite color is purple')
+})
+document.getElementById('place').addEventListener('click', function(){
+    alert('My favorite place is Colorado')
+})
+document.getElementById('ritual').addEventListener('click', function(){
+    alert('My favorite ritual is nightly meditation')
+})
+
